6e64f8f5057db582dd67b301b2c78142723df446
shashichilappagari <36974899+shashichilappagari@users.noreply.github.com>
2023-05-12 13:27:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,29 @@ import degirum as dg
 from PIL import Image
 
 zoo=dg.connect_model_zoo('https://cs.degirum.com/degirum_com/yolov5_cpu_nota',token=st.secrets["DG_TOKEN"])
+lp_det_model = zoo.load_model("yolo_v5s_lp_det--512x512_quant_n2x_orca_1")
+ocr_model = zoo.load_model("yolo_v5s_lp_ocr--256x256_quant_n2x_orca_1")
+
+# adjust some model properties
+lp_det_model.output_confidence_threshold = 0.7
+lp_det_model.overlay_font_scale = 1.5
+lp_det_model.overlay_alpha = 1
+ocr_model.overlay_show_labels = True
+ocr_model.overlay_font_scale = 1.5
+ocr_model.overlay_alpha = 1
+ocr_model.output_top_k = 1
  
-st.title('DeGirum Cloud Platform Demo of YOLOv5 Models')
-model_options=zoo.list_models()
-st.header('Choose and Run a Model')
-st.text('Select a model and upload an image. Then click on the submit button')
+st.title('Demo of License Plate Recognition using the DeGirum Cloud Platform')
+st.text('Upload an image. Then click on the submit button')
 with st.form("model_form"):
-    model_name=st.selectbox("Choose a Model from the list", model_options)
     uploaded_file=st.file_uploader('input image')
     submitted = st.form_submit_button("Submit")
     if submitted:
-        model=zoo.load_model(model_name)
-        model.overlay_font_scale=3
-        model.overlay_line_width=6
-        st.write("Model loaded successfully")
-        image = Image.open(uploaded_file)
-        predictions=model(image)
-        st.image(predictions.image_overlay,caption='Image with Bounding Boxes')
+        license_plates = lp_det_model(upploaded_file)
+        with ocr_model: # performance optimization to keep connection to mask_det_model open
+            for lp in license_plates.results:
+                lp_box = lp.image.crop(face['bbox'])
+                digits = ocr_model(lp_box)
+                lp["label"] = digits.results[0]["label"]
+        st.image(license_plates.image_overlay,caption='Image with License Plate Information')
             
